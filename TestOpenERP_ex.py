666903d582fb2e494b9dab85db61a16d14905d6e
Nacho Salvador <nachosalvador83@gmail.com>
2015-06-30 21:30:49 +0200

    Added an test-excercise

---
+++ TestOpenERP_ex.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+
+class TestOpenERP():
+
+	#---------------
+	#- First Tests -
+	#---------------
+
+	#---------
+	#- Tests -
+	#---------
+
+
+	#---------------
+	#- Final Tests -
+	#---------------
+
+
+	if  __name__ == '__main__':
+		runtests.main()

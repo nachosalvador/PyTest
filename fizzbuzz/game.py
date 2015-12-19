cf962999a92a43ff1e26bf4cf2dc166d7e8e58f5
Nacho Salvador <nachosalvador83@gmail.com>
2015-12-19 21:26:33 +0100

    Added game file

---
+++ fizzbuzz/game.py
@@ -0,0 +1,12 @@
+""" 
+Use: python game.py
+"""
+
+from fizzbuzz import FizzBuzz
+
+def game():
+    for number in range(1, 101):
+        print FizzBuzz().get(number)
+
+if  __name__ == '__main__':
+    game()

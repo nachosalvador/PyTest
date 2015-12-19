3d2911ca2c51598058068f2918bbc4ee46ca0381
Nacho Salvador <nachosalvador83@gmail.com>
2015-12-19 21:18:46 +0100

    Added fizzbuzz game

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,45 @@
+class FizzBuzz:
+    """ 
+    The FizzBuzz Game Class.
+    """
+
+    FIZZ = 'fizz'
+    BUZZ = 'buzz'
+
+    FIZZ_MULTIPLE = 3
+    BUZZ_MULTIPLE = 5
+
+    def get(self, number):
+        """ 
+        Gets the number.
+        """
+        return self._calculate_output(number)
+
+    def _calculate_output(self, number):
+        """ 
+        Calculates the number output.
+        """
+        output = number
+        
+        if self._is_fizz(number):
+            output = self.FIZZ
+        
+        if self._is_buzz(number):
+            if isinstance(output, str):
+                output += self.BUZZ
+            else:
+                output = self.BUZZ
+
+        return output
+
+    def _is_fizz(self, number):
+        """ 
+        Returns True if it is divisible by 3.
+        """
+        return number % self.FIZZ_MULTIPLE == 0
+
+    def _is_buzz(self, number):
+        """ 
+        Returns True if it is divisible by 5.
+        """
+        return number % self.BUZZ_MULTIPLE == 0
